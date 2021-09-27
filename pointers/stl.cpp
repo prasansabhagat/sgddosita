5e4bf02381da7c10f7f97aaee8e40fc5f0f77a07
Prasansa Bhagat <Prasansabhagat7@gmail.com>
2021-09-27 14:22:45 +0530

    n

--- pointers/stl.cpp
+++ pointers/stl.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include<deque>
 using namespace std;
 int main()
 {
-
+    //deque
+    deque<int> d;
+    d.push_back(3);
+    d.push_front(6);
+    for(int i:d)  cout<<i;
     return 0;
 }
